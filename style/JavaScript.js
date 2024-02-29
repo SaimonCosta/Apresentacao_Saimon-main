eaa228b650b251bb0cde9f7642e0a2737c3a497e
SaimonCosta <saimonsantoscosta@outlook.com>
2024-02-28 22:18:48 -0300

    Ajuste menu e outras paginas

--- style/JavaScript.js
+++ style/JavaScript.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var navList = document.getElementById('nav-list');
   
     button.addEventListener('click', function() {
-
+      // Alternar a visibilidade da lista de navegação ao clicar no botão
       navList.classList.toggle('show');
+      // Alternar o ícone do botão entre "Menu" e "X" com animação
+      if (navList.classList.contains('show')) {
+        button.textContent = 'X';
+        button.style.animation = 'changeIcon 0.5s ease forwards';
+      } else {
+        button.textContent = 'Menu';
+        button.style.animation = 'none'; // Remover a animação quando o texto voltar para "Menu"
+      }
     });
   });
